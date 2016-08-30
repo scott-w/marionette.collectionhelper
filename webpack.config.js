b4cd5727c7a5162564e275e4b2271adee00a1b71
Scott Walton <scott.walton@mypebble.co.uk>
2016-08-30 19:55:16 +0100

    Better webpack
    Improved the webpack config to not include jQuery and backbone.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  externals: {
+    jquery: {
+      global: '$',
+      commonjs: 'jquery'
+    },
+    backbone: true
+  },
   module: {
     loaders: [
       {
